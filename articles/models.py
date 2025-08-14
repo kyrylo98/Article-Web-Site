d7012edd29eaf7567579e6bfdb4763037d08e308
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-08-14 18:35:11 +0200

    create folder likes

---
+++ articles/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE, related_name='articles')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
---
+++ comments/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+from articles.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.article}"
+
